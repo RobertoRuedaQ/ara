cf5bc1d1bc9b5d8b562f63375440b13fe6675ee7
cmg <desarrollo@cmg.com.co>
2017-12-14 16:12:56 -0500

    hardcoding token

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -5,7 +5,7 @@ class Promo < ApplicationRecord
   def notification
   require 'fcm'
     fcm = FCM.new(Rails.application.secrets.firebase_server_token)
-    registration_ids = ['1']
+    registration_ids = ['eGSszGnR9rQ:APA91bGFQ4yVywRyxtmiUrURHpPlbPSdKgVy065vCNvN783YLzre-efQcaGsexoO0tIUJjiMym_17QWtzxXjQBw-h-yKtkcVkP0rG_cRxX_zq2b-vvAh9S9oUzj6cFp5mPeDCwr4r4fz']
     options = {data: {title: "test"}, collapse_key: "updated_score"}
     fcm.send(registration_ids, options)
   end
