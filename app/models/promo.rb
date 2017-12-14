69e893c7672aff57f8e051af2782b4bf7163e6b4
cmg <desarrollo@cmg.com.co>
2017-12-14 15:54:59 -0500

    wip

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -3,9 +3,10 @@ class Promo < ApplicationRecord
   mount_uploader :imagen, ImagenUploader
 
   def notification
-      require 'fcm'
-        fcm = FCM.new(Rails.application.secrets.firebase_server_token)
-        options = {data: {title: "test"}, collapse_key: "updated_score"}
-        fcm.send(options)
+  require 'fcm'
+    fcm = FCM.new(Rails.application.secrets.firebase_server_token)
+    registration_ids = ['1']
+    options = {data: {title: "test"}, collapse_key: "updated_score"}
+    fcm.send(registration_ids, options)
   end
 end
