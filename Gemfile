197af69ad41f4707b479e32b616579991f137e5a
cmg <desarrollo@cmg.com.co>
2017-12-18 13:55:58 -0500

    refine view for cellphone

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+gem 'google-api-client', '~> 0.11'
 gem 'fcm'
 gem 'fog'
 gem 'carrierwave', '~> 1.0'
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -2,11 +2,4 @@ class Promo < ApplicationRecord
   belongs_to :category
   mount_uploader :imagen, ImagenUploader
   validates :title, :imagen, :description, :category, :price, presence: true
-
-  def notification
-  require 'fcm'
-    fcm = FCM.new(Rails.application.secrets.firebase_server_token)
-		fcm.send_with_notification_key("/topics/test",
-            data: {title: "This is a FCM Topic Message!"})
-  end
 end
