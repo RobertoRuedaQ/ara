fc7a02bb133c00eaf243c26c7b92b4276091ead4
cmg <desarrollo@cmg.com.co>
2017-12-18 10:47:08 -0500

    include validations in promo model

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,7 +1,7 @@
 class Promo < ApplicationRecord
   belongs_to :category
   mount_uploader :imagen, ImagenUploader
-  validates :title, :imagen, :description, :category, :price
+  validates :title, :imagen, :description, :category, :price, presence: true
 
   def notification
   require 'fcm'
